aea32d1d808c311213054da7734aabdbcd34791b
Adam <toadam07@hotmail.com>
2026-06-29 23:02:49 -0600

    Added import for modelformset

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import modelformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import InventoryItem
