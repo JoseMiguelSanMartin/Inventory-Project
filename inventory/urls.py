8408bd2762646db5d076be978270ac2c75bcaecc
Aaron Kachmarski <aarokach@gmail.com>
2026-07-06 09:52:09 -0600

    fixed urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -20,9 +20,11 @@ urlpatterns = [
     path("create/", views.inventory_create, name="inventory_create"),
     path("<int:pk>/edit/", views.inventory_update, name="inventory_update"),
     path("<int:pk>/delete/", views.inventory_delete, name="inventory_delete"),
-    path("api/docs/", views.api_docs, name="api_docs"),
-    path("api/", include(router.urls)),
+
     path("daily-report/submit/", views.submit_daily_report, name="submit_daily_report"),
     path("daily-report/", views.daily_report, name="daily_report"),
     path("daily-report/<int:pk>/", views.daily_report_detail, name="daily_report_detail"),
+
+    path("api/docs/", views.api_docs, name="api_docs"),
+    path("api/", include(router.urls)),
 ]
