77cc244dbb3011d15efa05e184f7910c436749ab
Aaron Kachmarski <aarokach@gmail.com>
2026-07-11 12:44:25 -0600

    settings email setup

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,3 +73,18 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ]
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+DAILY_REPORT_RECIPIENT = os.environ.get(
+    "DAILY_REPORT_RECIPIENT",
+    ""
+)
